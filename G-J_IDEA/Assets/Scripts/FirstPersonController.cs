8c3ef3bb29e2fba3ab9f11ce9becaed170034067
JamesLXC <Jamscollins5@gmail.com>
2023-05-23 22:10:02 -0400

    Jumping
    Jumping Is now in

--- G-J_IDEA/Assets/Scripts/FirstPersonController.cs
+++ G-J_IDEA/Assets/Scripts/FirstPersonController.cs
@@ -1,56 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
- public bool CanMove {get; private set; } = true;
- private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
 
 
- [Header ("Functional Options")]
- [SerializeField] private bool canSprint = true;
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
 
- [Header("Controls")]
- [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
- [Header("Movement Parameters")]
- [SerializeField] private float walkSpeed = 3.0f;
- [SerializeField] private float sprintSpeed = 6.0f;
- [SerializeField] private float gravity = 30.0f;
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
 
- [Header("look Parameters")]
- [SerializeField, Range (1,10)] private float lookSpeedX = 2.0f;
- [SerializeField, Range (1,10)] private float lookSpeedY = 2.0f;
- [SerializeField, Range (1,180)] private float upperLookLimit = 80.0f;
- [SerializeField, Range (1,80)] private float lowerLookLimit = 80.0f;
+    [Header("look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 80)] private float lowerLookLimit = 80.0f;
 
- private Camera playerCamera;
- private CharacterController characterController;
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
 
- private Vector3 moveDirection;
- private Vector2 currentIput;
+    private Camera playerCamera;
+    private CharacterController characterController;
 
- private float roationX = 0;
-   
-  void Awake()
-   {
+    private Vector3 moveDirection;
+    private Vector2 currentIput;
+
+    private float roationX = 0;
+
+    void Awake()
+    {
         playerCamera = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-   }
+    }
 
     private void Update()
     {
         if (CanMove)
         {
-             HandleMovementInput();
-             HandleMOuseLook();
+            HandleMovementInput();
+            HandleMOuseLook();
+           
+            if (canJump)
+            {
+                HandleJump();
+            }
 
-             ApplyFinalMovements();
-
+            ApplyFinalMovements();
         }
     }
     private void HandleMovementInput()
@@ -72,10 +79,15 @@ public class FirstPersonController : MonoBehaviour
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
     }
 
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            moveDirection.y = jumpForce;
+    }
 
     private void ApplyFinalMovements()
     {
-        if(!characterController.isGrounded)
+        if (!characterController.isGrounded)
             moveDirection.y -= gravity * Time.deltaTime;
 
         characterController.Move(moveDirection * Time.deltaTime);
